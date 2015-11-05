70146b5f0777d209c47902473fd8b4cffc859d3e
Ichthus-Marty <mvdh22@gmail.com>
2015-11-05 13:55:22 +0100

    Initial upload
    The first upload for this project, includes file that were made before
    Github was used

---
+++ Code/AGM test/build/obj/AGM test_main.cpp.d
@@ -0,0 +1,16 @@
+ \
+ c:\users\martijn\documents\github\projectdh\code\AGM\ test\build\obj\AGM\ test_main.cpp.o:  \
+ c:\users\martijn\documents\github\projectdh\code\AGM\ test\source\AGM\ test_main.cpp \
+  c:\users\martijn\documents\github\projectdh\code\AGM\ test\source\/AGM\ test_main.h \
+  C:\Arduino\hardware\arduino\cores\arduino/arduino.h \
+  C:\Arduino\hardware\arduino\cores\arduino/binary.h \
+  C:\Arduino\hardware\arduino\cores\arduino/WCharacter.h \
+  C:\Arduino\hardware\arduino\cores\arduino/WString.h \
+  C:\Arduino\hardware\arduino\cores\arduino/HardwareSerial.h \
+  C:\Arduino\hardware\arduino\cores\arduino/Stream.h \
+  C:\Arduino\hardware\arduino\cores\arduino/Print.h \
+  C:\Arduino\hardware\arduino\cores\arduino/Printable.h \
+  C:\Arduino\hardware\arduino\cores\arduino/new.h \
+  C:\Arduino\hardware\arduino\variants\standard/pins_arduino.h \
+  C:\Arduino\libraries\Wire/Wire.h \
+  C:\Arduino\hardware\arduino\cores\arduino/Stream.h
