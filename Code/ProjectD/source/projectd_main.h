fa2ef9f4aac31fe6b231e76a3a34eeaef8a884e1
Ichthus-Marty <mvdh22@gmail.com>
2015-11-19 18:13:44 +0100

    Sonar lib toegevoegd

--- Code/ProjectD/source/projectd_main.h
+++ Code/ProjectD/source/projectd_main.h
@@ -2,25 +2,28 @@
 #ifndef __projectd_main_H__
 #define __projectd_main_H__
 //-------------------------------------------------------------------
-
+ 
 #include <arduino.h>
-
+//#include "gam.h"
+//#include <Wire.h>
+#include <twi.h>
 //-------------------------------------------------------------------
-
+ 
 //-------------------------------------------------------------------
-
-// Put yout declarations here
-
+ 
+#include <Wire.h>
+#include <twi.h>
+ 
 //-------------------------------------------------------------------
-
+ 
 //===================================================================
 // -> DO NOT WRITE ANYTHING BETWEEN HERE...
 // 		This section is reserved for automated code generation
 // 		This process tries to detect all user-created
-// 		functions in main_sketch.cpp, and inject their  
+// 		functions in main_sketch.cpp, and inject their
 // 		declarations into this file.
-// 		If you do not want to use this automated process,  
-//		simply delete the lines below, with "&MM_DECLA" text 
+// 		If you do not want to use this automated process,
+//		simply delete the lines below, with "&MM_DECLA" text
 //===================================================================
 //---- DO NOT DELETE THIS LINE -- @MM_DECLA_BEG@---------------------
 void loop();
@@ -28,8 +31,12 @@ void setup();
 //---- DO NOT DELETE THIS LINE -- @MM_DECLA_END@---------------------
 // -> ...AND HERE. This space is reserved for automated code generation!
 //===================================================================
-
-
+ 
+ 
 //-------------------------------------------------------------------
 #endif
 //-------------------------------------------------------------------
+ 
+ 
+ 
+ 
