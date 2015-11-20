f49167d4a29ce53f3e2d0fe5147ed08b257de12a
Ichthus-Marty <mvdh22@gmail.com>
2015-11-20 19:39:43 +0100

    Build files previous commit
    Forgot to update the buildfiles from the last commit

--- Code/ProjectD/build/obj/gam_sensors.cpp.d
+++ Code/ProjectD/build/obj/gam_sensors.cpp.d
@@ -1,7 +1,8 @@
  \
- c:\users\martijn\documents\github\droneglove\code\ProjectD\build\obj\morse.cpp.o:  \
- c:\users\martijn\documents\github\droneglove\code\ProjectD\source\morse.cpp \
-  C:\Arduino\hardware\arduino\cores\arduino/Arduino.h \
+ c:\users\martijn\documents\github\droneglove\code\ProjectD\build\obj\gam_sensors.cpp.o:  \
+ c:\users\martijn\documents\github\droneglove\code\ProjectD\source\gam_sensors.cpp \
+  c:\users\martijn\documents\github\droneglove\code\ProjectD\source\/GAM_Sensors.h \
+  C:\Arduino\hardware\arduino\cores\arduino/arduino.h \
   C:\Arduino\hardware\arduino\cores\arduino/binary.h \
   C:\Arduino\hardware\arduino\cores\arduino/WCharacter.h \
   C:\Arduino\hardware\arduino\cores\arduino/WString.h \
@@ -11,4 +12,5 @@
   C:\Arduino\hardware\arduino\cores\arduino/Printable.h \
   C:\Arduino\hardware\arduino\cores\arduino/new.h \
   C:\Arduino\hardware\arduino\variants\standard/pins_arduino.h \
-  c:\users\martijn\documents\github\droneglove\code\ProjectD\source\/Morse.h
+  C:\Arduino\libraries\Wire\Wire.h \
+  C:\Arduino\hardware\arduino\cores\arduino/Stream.h
